a6aa0014e89250b7aa1f3cb8de8807fc23f38b5b
Omar A <oazzat19@gmail.com>
2018-10-01 23:27:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,6 @@ def adding_to_dennis
         }
      }
      
-     return programmer_hash[:dennis_ritchie] [:languages][1]
+     return programmer_hash [:dennis_ritchie] [:languages] [1]
 
 end
